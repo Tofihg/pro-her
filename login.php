7cf550cf5fba202d61549acc5d5668bc3a5e15fd
Oday-Alkabra <124041469+Oday-Alkabra@users.noreply.github.com>
2024-03-25 12:59:08 +0100

    Update login.php
    Taal fouten

--- login.php
+++ login.php
@@ -49,7 +49,7 @@ if(isset($message)){
 
    <form action="" method="post">
       <h3>login Nu</h3>
-      <input type="email" name="email" required placeholder="enter email" class="box">
+      <input type="email" name="email" required placeholder="Email" class="box">
       <input type="password" name="password" required placeholder="enter password" class="box">
       <input type="submit" name="submit" class="btn" value="login now">
       <p>heb geen account? <a href="register.php">Registreer nu</a></p>
